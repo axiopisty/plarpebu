5795380a2bd61f4e47c12f76b4a4704d043f69b1
Kevin Schmidt <kevin.m.schmidt@fuse.net>
2006-02-28 15:42:18 +0000

    Cleanup
    git-svn-id: http://svn.code.sf.net/p/plarpebu/code@74 08b1f3f0-860d-0410-8033-80c2cf11100b

--- src/plugins/misc/albumgrabber/AlbumGrabberPlugin.java
+++ src/plugins/misc/albumgrabber/AlbumGrabberPlugin.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import java.net.MalformedURLException;
 import java.util.Map;
 
 import javax.swing.ImageIcon;
@@ -46,7 +45,7 @@ public class AlbumGrabberPlugin extends FramePlugin implements ActionListener, B
 
     private String albumTitle = null;
 
-    public AlbumGrabberPlugin() throws MalformedURLException {
+    public AlbumGrabberPlugin() {
 
         popupListener = new PopupListener();
         this.addMouseListener(popupListener);
